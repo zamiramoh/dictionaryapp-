bbc48cd94425646045361c18f1e4d4846d5e7240
zamira <samiiramaxamed277@gmail.com>
2025-02-04 13:11:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace dictionaryappa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstWords = new System.Windows.Forms.ListBox();
+            this.lblMeaning = new System.Windows.Forms.Label();
+            this.btnspeak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(57, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Dictionary";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(25, 48);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(324, 51);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Cyan;
+            this.btnSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnSearch.Location = new System.Drawing.Point(346, 48);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 51);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstWords
+            // 
+            this.lstWords.BackColor = System.Drawing.Color.IndianRed;
+            this.lstWords.FormattingEnabled = true;
+            this.lstWords.ItemHeight = 20;
+            this.lstWords.Location = new System.Drawing.Point(25, 106);
+            this.lstWords.Name = "lstWords";
+            this.lstWords.Size = new System.Drawing.Size(231, 224);
+            this.lstWords.TabIndex = 3;
+            // 
+            // lblMeaning
+            // 
+            this.lblMeaning.AutoSize = true;
+            this.lblMeaning.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMeaning.Location = new System.Drawing.Point(88, 339);
+            this.lblMeaning.Name = "lblMeaning";
+            this.lblMeaning.Size = new System.Drawing.Size(103, 28);
+            this.lblMeaning.TabIndex = 4;
+            this.lblMeaning.Text = "Meaning";
+            // 
+            // btnspeak
+            // 
+            this.btnspeak.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnspeak.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnspeak.Location = new System.Drawing.Point(262, 327);
+            this.btnspeak.Name = "btnspeak";
+            this.btnspeak.Size = new System.Drawing.Size(86, 47);
+            this.btnspeak.TabIndex = 5;
+            this.btnspeak.Text = " 🔊";
+            this.btnspeak.UseVisualStyleBackColor = false;
+            this.btnspeak.Click += new System.EventHandler(this.btnspeak_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(437, 368);
+            this.Controls.Add(this.btnspeak);
+            this.Controls.Add(this.lblMeaning);
+            this.Controls.Add(this.lstWords);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "My Dictionary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lstWords;
+        private System.Windows.Forms.Label lblMeaning;
+        private System.Windows.Forms.Button btnspeak;
+    }
+}
+
